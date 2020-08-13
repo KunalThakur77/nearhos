a50d749d791586284a05440b9e0af666f0aed0e4
KunalThakur77 <kunalt726@gmail.com>
2020-08-13 10:34:22 +0530

    hey

--- server.js
+++ server.js
@@ -11,26 +11,42 @@ app.listen( process.env.PORT ||3000,function(){
 });
 
 app.get('/', function(req, res) {
-  res.render('index')
-});
-app.get('/t', function(req, res) {
-  res.render('indexo')
-});
-app.post("/bck",function(req,res){
-  const a=req.body.lat
-  const b=req.body.lon
-  console.log(a)
-  console.log(b)
-    axios.get('http://dev.virtualearth.net/REST/V1/Routes/LocalInsights?waypoint='+req.body.lat+','+req.body.lon+'&TravelMode=Driving&Optimize=time&MaxTime=20&TimeUnit=Minute&type=Hospitals&key=Akm-NUKYwLBqtU3z7n7uftlnRXC6iv55a9VqDZEkxLas1QkYTQeOTn3Isr0MRP9w')
-    .then(function (response) {
-      const heading=response.data.resourceSets[0].resources[0].categoryTypeResults[0].categoryTypeSummary
-      const base = response.data.resourceSets[0].resources[0].categoryTypeResults[0].entities
-      mza=[]
-      const l = base.length;
-      for (i = 0; i < base.length; i++) {
-      mza.push(base[i])
+  axios.get('http://api.ipstack.com/check?access_key=47692fd4f78ff8a355e5479f6f976edf')
+  .then(function(respo){
+    console.log(respo.data.city)
+    const ci = respo.data.city
+    const zi = respo.data.zip
+  axios.get('http://dev.virtualearth.net/REST/V1/Routes/LocalInsights?waypoint='+ci+'&postalcode='+zi+'&TravelMode=Driving&Optimize=time&MaxTime=20&TimeUnit=Minute&type=Hospitals&key=Akm-NUKYwLBqtU3z7n7uftlnRXC6iv55a9VqDZEkxLas1QkYTQeOTn3Isr0MRP9w')
+  .then(function (response) {
+    const heading=response.data.resourceSets[0].resources[0].categoryTypeResults[0].categoryTypeSummary
+    const base = response.data.resourceSets[0].resources[0].categoryTypeResults[0].entities
+    mza=[]
+    const l = base.length;
+    for (i = 0; i < base.length; i++) {
+    mza.push(base[i])
 }
-res.render('indexo',{array:mza,hd:heading})
-    })
-
+res.render('indexo',{array:mza,hd:heading,oho:ci})
+  })
+  })
 });
+// app.get('/t', function(req, res) {
+//   res.render('indexo')
+// });
+// app.post("/bck",function(req,res){
+//   const a=req.body.lat
+//   const b=req.body.lon
+//   console.log(a)
+//   console.log(b)
+//     axios.get('http://dev.virtualearth.net/REST/V1/Routes/LocalInsights?waypoint='+req.body.lat+','+req.body.lon+'&TravelMode=Driving&Optimize=time&MaxTime=20&TimeUnit=Minute&type=Hospitals&key=Akm-NUKYwLBqtU3z7n7uftlnRXC6iv55a9VqDZEkxLas1QkYTQeOTn3Isr0MRP9w')
+//     .then(function (response) {
+//       const heading=response.data.resourceSets[0].resources[0].categoryTypeResults[0].categoryTypeSummary
+//       const base = response.data.resourceSets[0].resources[0].categoryTypeResults[0].entities
+//       mza=[]
+//       const l = base.length;
+//       for (i = 0; i < base.length; i++) {
+//       mza.push(base[i])
+// }
+// res.render('indexo',{array:mza,hd:heading})
+//     })
+//
+// });
