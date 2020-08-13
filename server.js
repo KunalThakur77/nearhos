5c4840a6ad9d92c9d07ce3e6682ee5ffaeb8a321
KunalThakur77 <kunalt726@gmail.com>
2020-08-13 10:59:31 +0530

    jkjwdbf

--- server.js
+++ server.js
@@ -11,22 +11,27 @@ app.listen( process.env.PORT ||3000,function(){
 });
 
 app.get('/', function(req, res) {
-  axios.get('http://api.ipstack.com/check?access_key=47692fd4f78ff8a355e5479f6f976edf')
-  .then(function(respo){
-    const ci = respo.data.city
-    const zi = respo.data.zip
-  axios.get('http://dev.virtualearth.net/REST/V1/Routes/LocalInsights?waypoint='+ci+'&postalcode='+zi+'&TravelMode=Driving&Optimize=time&MaxTime=20&TimeUnit=Minute&type=Hospitals&key=Akm-NUKYwLBqtU3z7n7uftlnRXC6iv55a9VqDZEkxLas1QkYTQeOTn3Isr0MRP9w')
-  .then(function (response) {
-    const heading=response.data.resourceSets[0].resources[0].categoryTypeResults[0].categoryTypeSummary
-    const base = response.data.resourceSets[0].resources[0].categoryTypeResults[0].entities
-    mza=[]
-    const l = base.length;
-    for (i = 0; i < base.length; i++) {
-    mza.push(base[i])
-}
-res.render('indexo',{array:mza,hd:heading,oho:ci})
-  })
-  })
+//   axios.get('http://api.ipstack.com/check?access_key=47692fd4f78ff8a355e5479f6f976edf')
+//   .then(function(respo){
+//     const ci = respo.data.city
+//     const zi = respo.data.zip
+//   axios.get('http://dev.virtualearth.net/REST/V1/Routes/LocalInsights?waypoint='+ci+'&postalcode='+zi+'&TravelMode=Driving&Optimize=time&MaxTime=20&TimeUnit=Minute&type=Hospitals&key=Akm-NUKYwLBqtU3z7n7uftlnRXC6iv55a9VqDZEkxLas1QkYTQeOTn3Isr0MRP9w')
+//   .then(function (response) {
+//     const heading=response.data.resourceSets[0].resources[0].categoryTypeResults[0].categoryTypeSummary
+//     const base = response.data.resourceSets[0].resources[0].categoryTypeResults[0].entities
+//     mza=[]
+//     const l = base.length;
+//     for (i = 0; i < base.length; i++) {
+//     mza.push(base[i])
+// }
+// res.render('indexo',{array:mza,hd:heading,oho:ci})
+//   })
+//   })
+axios.get('https://api.myip.com/')
+.then(function(response){
+  console.log(response.data.ip)
+  res.render('index',{i:response.data.ip})
+})
 });
 // app.get('/t', function(req, res) {
 //   res.render('indexo')
